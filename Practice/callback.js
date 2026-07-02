bf44a1b99f635f1d1d9fbf9acd7cc712228ad45c
Arman Shekhu <armanshekhu786@gmail.com>
2026-07-02 08:36:14 +0530

    Refactor callback functions for improved readability and functionality

--- Practice/callback.js
+++ Practice/callback.js
@@ -6,13 +6,13 @@
 //Synchronous callback - that is executed immediately after being called, blocking the 
 // execution of further code until it completes.
 
-// function greet(name) {
-//     console.log(`Hello, ${name}!`);
-// }
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
 
-// function callGreet(callback) {
-//     callback();
-// }
+function callGreet(callback) {
+    callback();
+}
 
 // callGreet(() => greet("Arman"));
 // callGreet(greet("Arman"));
@@ -23,52 +23,87 @@
 // operation has completed, allowing other code to run in the meantime.
 
 
-// function position(str) {
-//     console.log(`reached ${str}!`);
-// }
+function position(str) {
+    console.log(`reached ${str}!`);
+}
 
-// function race(callback) {
-//     console.log("Race started");
-//     setTimeout(() => {
-//         callback();
-//     }, 2000);
-// }
+function race(callback) {
+    console.log("Race started");
+    setTimeout(() => {
+        callback();
+    }, 2000);
+}
 
 // race(() => position("middle"));    
 // race(() => position("end")); 
 
-
-function login() {
-    console.log("1.Logging in...");
+function login(callback) {
+    console.log("1. Logging in...");
 
     setTimeout(() => {
-        console.log("2.Logged in successfully!");
+        console.log("2. Logged in successfully!");
+        callback();
     }, 5000);
 }
 
 function getUser(callback) {
-    console.log("3.Getting user...");
+    console.log("3. Getting user...");
+
     setTimeout(() => {
-        console.log("4.User is loaded!");
+        console.log("4. User is loaded!");
+        callback();
     }, 2000);
 }
 
-function getOrders() {
-    console.log("5.Getting orders...");
+function getOrders(callback) {
+    console.log("5. Getting orders...");
+
     setTimeout(() => {
-        console.log("6.Orders are loaded!");
+        console.log("6. Orders are loaded!");
+        callback();
     }, 1000);
 }
 
-function getOrderData() {
-    console.log("7.Getting order data...");
+function getOrderData(callback) {
+    console.log("7. Getting order data...");
+
     setTimeout(() => {
-        console.log("8.Order data is loaded!");
+        console.log("8. Order data is loaded!");
     }, 3000);
 }
 
-login(
-    
+// login(() => {
+//     getUser(() => {
+//         getOrders(() => {
+//             getOrderData();
+//         });
+//     });
+// });
+
+//Arrow function 
+
+function sum(a,b) {
+    console.log(a+b);
+}
+
+// sum(5,10);
+
+const sumArrow = (a,b) => {
+    console.log(a+b);
+}
+
+// sumArrow(5,10);
 
 
+let numbers = [1,2,3,4,5];
 
+numbers.forEach((number) => {
+    console.log(number);
+})
+
+
+let EvenNumbers = numbers.map((number) => {
+    return number * 2;
+});
+
+console.log(EvenNumbers);
