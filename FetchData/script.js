743f2c5b7555b77c5c271c222d078061f058c823
Arman Shekhu <armanshekhu786@gmail.com>
2026-07-07 11:14:16 +0530

    Fix HTML structure and add event listener for fetching posts

--- FetchData/script.js
+++ FetchData/script.js
@@ -1,2 +1,14 @@
 const postSection = document.querySelector("#posts")
-const 
+const btn1 = document.querySelector("#btn1")
+
+const h2 = document.createElement('h2')
+h2.innerText = 'Posts';
+
+const fetchData = async () => {
+    const data = await fetch('https://jsonplaceholder.tyicode.com/posts')
+
+    const todos = await data.json()
+    console.log(todos);
+}
+
+btn1.addEventListener('click',fetchData)
