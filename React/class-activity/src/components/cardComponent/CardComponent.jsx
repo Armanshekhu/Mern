82f2f0817accfd21a463d74d778948ffa0695cf6
Arman Shekhu <armanshekhu786@gmail.com>
2026-07-15 17:37:59 +0530

    feat: add CardComponent with styling

---
+++ React/class-activity/src/components/cardComponent/CardComponent.jsx
@@ -0,0 +1,16 @@
+import "./CardComponent.css";
+
+function CardComponent(props) {
+
+    return (
+        <div className="card">
+            <img src={props.image} alt="Course Image" />
+            <p>{props.title}</p>
+            <p>Instructor: {props.instructor}</p>
+            <p>Price: {props.price}</p>
+            <p>Level: {props.level}</p>
+        </div>
+    )
+}
+
+export default CardComponent;
