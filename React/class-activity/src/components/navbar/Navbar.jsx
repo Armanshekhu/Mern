d3a51e18b643777edbc30b72307debfba181e3ba
Arman Shekhu <armanshekhu786@gmail.com>
2026-07-15 17:40:00 +0530

    feat: update Navbar and Dashboard components with styling and functionality

--- React/class-activity/src/components/navbar/Navbar.jsx
+++ React/class-activity/src/components/navbar/Navbar.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ThemeContext } from "../../context/ThemeContext";
+import "./navbar.css";
+
 function Navbar() {
+    const { toggleTheme } = useContext(ThemeContext);
+
     return (
-        <div>
-            <p>This is Navbar Component</p>
+        <div className='navbar'>
+            <Link to='/dashboard'>Dashboard</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/signup'>Signup</Link>
+            <Link to='/about'>About</Link>
+            <button onClick={() => toggleTheme()}>Toggle</button>
         </div>
     )
 }
--- React/class-activity/src/pages/dashboard/Dashboard.jsx
+++ React/class-activity/src/pages/dashboard/Dashboard.jsx
@@ -1,10 +1,24 @@
+import CardComponent from "../../components/cardComponent/CardComponent";
+import courses from "../../data/courses";
+import "./dashboard.css";
 
 function Dashboard() {
     return (
         <div>
             <p>This is Dashboard Page</p>
+            {courses.map((course) => (
+                <CardComponent
+                    key={course.id}
+                    image={course.image}
+                    title={course.title}
+                    instructor={course.instructor}
+                    price={course.price}
+                    level={course.level}
+                />
+            ))} 
+            
         </div>
-    )
+    );
 }
 
 export default Dashboard;
